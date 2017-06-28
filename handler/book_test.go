ea06bdf26742e4fb391d6f24e0932a5faa5182b0
kaichao <kaichaosuna@gmail.com>
2017-06-28 17:57:47 +0800

    cover test for book handler.

--- handler/book_test.go
+++ handler/book_test.go
@@ -10,6 +10,7 @@ import (
 	. "github.com/smartystreets/goconvey/convey"
 	"encoding/json"
 	"encoding/xml"
+	"strings"
 )
 
 func TestShowIndexPage(t *testing.T) {
@@ -48,6 +49,53 @@ func TestShowIndexPage(t *testing.T) {
 			err := xml.Unmarshal(p, &books)
 			So(w.Code, ShouldEqual, http.StatusOK)
 			So(err, ShouldBeNil)
+		})
+	})
+}
+
+func TestGetBook(t *testing.T) {
+	r := utility.GetRouter(true)
+	r.GET("/book/:book_id", GetBook)
+
+	Convey("The book id is not integer", t, func() {
+		req, _ := http.NewRequest("GET", "/book/a", nil)
+
+		utility.TestHTTPResponse(r, req, func(w *httptest.ResponseRecorder) {
+			So(w.Code, ShouldEqual, http.StatusBadRequest)
+		})
+	})
+
+	Convey("The book exists", t, func() {
+		req, _ := http.NewRequest("GET", "/book/10", nil)
+
+		utility.TestHTTPResponse(r, req, func(w *httptest.ResponseRecorder) {
+			p, _ := ioutil.ReadAll(w.Body)
+			So(w.Code, ShouldEqual, http.StatusOK)
+			So(string(p), ShouldContainSubstring, "<title>Book</title>")
+		})
+	})
+}
+
+func TestSaveBook(t *testing.T) {
+	r := utility.GetRouter(true)
+	r.POST("/book", SaveBook)
+
+	Convey("The params can not convert to model book", t, func() {
+		req, _ := http.NewRequest("POST", "/book", nil)
+		req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+
+		utility.TestHTTPResponse(r, req, func(w *httptest.ResponseRecorder) {
+			So(w.Code, ShouldEqual, http.StatusBadRequest)
+		})
+	})
+
+	Convey("The params can convert to model book", t, func() {
+		req, _ := http.NewRequest("POST", "/book", strings.NewReader("title=Hello world&author=will"))
+		req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+		utility.TestHTTPResponse(r, req, func(w *httptest.ResponseRecorder) {
+			p, _ := ioutil.ReadAll(w.Body)
+			So(w.Code, ShouldEqual, http.StatusOK)
+			So(string(p), ShouldContainSubstring, "保存成功")
 		})
 	})
 }
