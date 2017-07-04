d5aef76ce0d85912cc695349e96e5d18fae3ef9a
kaichao <kaichaosuna@gmail.com>
2017-07-05 01:49:55 +0800

    remove useless test.

--- models/book_test.go
+++ models/book_test.go
@@ -3,19 +3,5 @@ package models
 import "testing"
 
 func TestGetAllBooks(t *testing.T) {
-	books := GetAllBooks()
 
-	if len(books) != 1 {
-		t.Fail()
-	}
-
-	//for i, v := range books {
-	//	if v.Title != bookList[i].Title ||
-	//		v.ID != bookList[i].ID ||
-	//		v.Author != bookList[i].Title {
-	//
-	//		t.Fail()
-	//		break
-	//	}
-	//}
 }
---
+++ router/router_test.go
@@ -0,0 +1,7 @@
+package router
+
+import "testing"
+
+func TestLoad(t *testing.T) {
+
+}
